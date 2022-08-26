05ed4e5e10d41b14e8cc51f6d2d881e2747f50e1
agutierrezeye <109681476+agutierrezeye@users.noreply.github.com>
2022-08-26 11:29:51 -0500

    v1.0

--- speed.py
+++ speed.py
@@ -5,20 +5,20 @@ import ssl
 
 while True:
 	try:
-	    ssl._create_default_https_context = ssl._create_unverified_context
-	    st = speedtest.Speedtest()
+		ssl._create_default_https_context = ssl._create_unverified_context
+		st = speedtest.Speedtest()
 
 
-	    download_speed = st.download()
-	    upload_speed = st.upload(pre_allocate=False)
+		download_speed = st.download()
+		upload_speed = st.upload(pre_allocate=False)
 
 
-	    print('Download Speed: {:5.2f} Mb'.format(download_speed/(1024*1024)))
-	    print('Upload Speed: {:5.2f} Mb'.format(upload_speed/(1024*1024)))
-	    f = open("logspeed.txt",'a+')
-	    f.write(str(download_speed/(1024*1024))+" "+str(upload_speed/(1024*1024))+"\n")
-	    f.close()
+		print('Download Speed: {:5.2f} Mb'.format(download_speed/(1024*1024)))
+		print('Upload Speed: {:5.2f} Mb'.format(upload_speed/(1024*1024)))
+		f = open("logspeed.txt",'a+')
+		f.write(str(download_speed/(1024*1024))+" "+str(upload_speed/(1024*1024))+"\n")
+		f.close()
 
 	except:
-	   print("Error de conexion con server")
-	   True
+		print("Error de conexion con server")
+		True
